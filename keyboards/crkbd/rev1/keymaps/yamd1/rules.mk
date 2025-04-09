30e7b7fe0930bace23ca85785ba5986d3dfe6613
yamd1 <w5000c2d26@gmail.com>
2025-04-09 09:48:04 +0900

    📝 コンパイル対象に関する説明を追加

--- keyboards/crkbd/rev1/keymaps/yamd1/rules.mk
+++ keyboards/crkbd/rev1/keymaps/yamd1/rules.mk
@@ -3,7 +3,9 @@ OLED_ENABLE = yes
 COMBO_ENABLE=yes
 HOME_ROW_COMBO_EBABLE=yes
 CAPS_WORD_ENABLE = yes
-CONVERT_TO=elite_pi
+
+# コンパイル対象を指定する。コメントアウトするとavr用にコンパイルされる
+# CONVERT_TO=elite_pi
 
 # key_overridesが未定義の場合によって引き起こされるコンパイルエラーへ対応
 # SEE: https://chatgpt.com/c/67f5b8b9-07e8-8009-b9d8-4e1dd24f6ec1
